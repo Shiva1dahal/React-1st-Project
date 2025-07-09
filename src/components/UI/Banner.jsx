592a2f9a6c2d8d2afaa8639c5376b08aae1f849c
Shiva Dahal <dahalshiva427@gmail.com>
2025-07-09 14:17:03 +0545

    connected images of such that it shows when deployed

--- src/components/UI/Banner.jsx
+++ src/components/UI/Banner.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom';
-
+import Imageone from '../../assets/Books.jpg'
 function Banner({title,subtitle,description}) {
     const location=useLocation()
     console.log(location)
@@ -8,7 +8,7 @@ function Banner({title,subtitle,description}) {
     <div className='h-screen bg-red-500 '>
 
       <div className='h-full relative'>
-        <img src="./src/assets/Books.jpg"  className='h-full w-full object-cover' />
+        <img src={Imageone} className='h-full w-full object-cover' />
         <div className='absolute top-0 w-full h-full flex justify-center  items-center flex-col left-0 right-0 bg-black/30 z-10' >
           <div className='text-red-500'>Welcome to {title}</div>
           <div className='text-red-500' >{subtitle}</div>
